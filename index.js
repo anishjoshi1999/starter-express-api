0b774b600528cc01fc3010f19862b55139a9c35e
anishjoshi1999 <anishjoshi1999@gmail.com>
2023-09-17 19:48:13 +0545

    Facebook API

--- index.js
+++ index.js
@@ -1,7 +1,39 @@
-const express = require('express')
-const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
-})
-app.listen(process.env.PORT || 3000)
+const express = require("express");
+const app = express();
+const axios = require("axios");
+require("dotenv").config();
+
+app.post("/facebook", async (req, res) => {
+  try {
+    // Fetch a meme from MEME_URI
+    const memeResponse = await axios.get(process.env.MEME_URI);
+    const { title, url } = memeResponse.data;
+
+    // Add a caption to the meme
+    const caption = `${title}`;
+
+    // Post the meme with caption to Facebook
+    const facebookResponse = await postMemeToFacebook(url, caption);
+
+    console.log("Automatic POST request triggered:", facebookResponse.data);
+    res.status(200).json({ message: "Meme posted on Facebook successfully" });
+  } catch (error) {
+    console.error("An error occurred:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+async function postMemeToFacebook(url, caption) {
+  const facebookUrl = `https://graph.facebook.com/${process.env.FACEBOOK_PAGE_ID}/photos`;
+  const data = {
+    url,
+    caption, // Include the caption here
+    access_token: process.env.FACEBOOK_PAGE_ACCESS_TOKEN,
+  };
+
+  return axios.post(facebookUrl, data);
+}
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`Listening on PORT ${process.env.PORT}`);
+});
